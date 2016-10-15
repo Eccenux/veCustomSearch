72fa1b37d27b8d3c8d1aa83e77c9332f40c28d69
Eccenux <eccenux+git@gmail.com>
2016-10-15 19:19:10 +0200

    localhost + logging

--- veCustomSearch.loader.js
+++ veCustomSearch.loader.js
@@ -2,10 +2,12 @@
 	Script loader for an actual plugin
 */
 // load User Script
-mw.loader.using( 'ext.visualEditor.desktopArticleTarget.init', function () {
+mw.loader.using('ext.visualEditor.desktopArticleTarget.init', function () {
+	console.log('init');
 	// Register plugins to VE. will be loaded once the user opens VE
-	mw.libs.ve.addPlugin( function () { 
+	mw.libs.ve.addPlugin(function () {
+		console.log('addPlugin');
 		//return $.getScript('//pl.wikipedia.org/w/index.php?title=User:Nux/veCustomSearch.plugin.js&action=raw&ctype=text/javascript'); 
-		return $.getScript('/_wiki_js/_przyciski%20edycji/veMyPlugins/veCustomSearch.plugin.js'); 
-	} );
+		return $.getScript('https://localhost/_wiki_js/_przyciski%20edycji/veMyPlugins/veCustomSearch.plugin.js'); 
+	});
 });
