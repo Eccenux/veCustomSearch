5ff3a00e31bbd188b3e2f85acb2e89ca6dd28cf6
Eccenux <eccenux+git@gmail.com>
2016-10-15 21:50:52 +0200

    correct port

--- veCustomSearch.loader.js
+++ veCustomSearch.loader.js
@@ -7,7 +7,7 @@ mw.loader.using('ext.visualEditor.desktopArticleTarget.init', function () {
 	// Register plugins to VE. will be loaded once the user opens VE
 	mw.libs.ve.addPlugin(function () {
 		console.log('addPlugin');	// only happens on-switch to VE (not when VE is already loaded)
-		//return $.getScript('//pl.wikipedia.org/w/index.php?title=User:Nux/veCustomSearch.plugin.js&action=raw&ctype=text/javascript'); 
-		return $.getScript('https://localhost/_wiki_js/_przyciski%20edycji/veMyPlugins/veCustomSearch.plugin.js'); 
+		//return $.getScript('//pl.wikipedia.org/w/index.php?title=User:Nux/veCustomSearch.plugin.js&action=raw&ctype=text/javascript');
+		return $.getScript('https://localhost:8443/_wiki_js/_przyciski%20edycji/veMyPlugins/veCustomSearch.plugin.js');
 	});
 });
