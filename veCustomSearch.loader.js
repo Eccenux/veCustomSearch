5e2f284f4fd4079abe338593271924a220b62d38
Eccenux <eccenux+git@gmail.com>
2016-10-15 19:19:40 +0200

    events occurrence info

--- veCustomSearch.loader.js
+++ veCustomSearch.loader.js
@@ -3,10 +3,10 @@
 */
 // load User Script
 mw.loader.using('ext.visualEditor.desktopArticleTarget.init', function () {
-	console.log('init');
+	console.log('init');	// happens when VE is already loaded too
 	// Register plugins to VE. will be loaded once the user opens VE
 	mw.libs.ve.addPlugin(function () {
-		console.log('addPlugin');
+		console.log('addPlugin');	// only happens on-switch to VE (not when VE is already loaded)
 		//return $.getScript('//pl.wikipedia.org/w/index.php?title=User:Nux/veCustomSearch.plugin.js&action=raw&ctype=text/javascript'); 
 		return $.getScript('https://localhost/_wiki_js/_przyciski%20edycji/veMyPlugins/veCustomSearch.plugin.js'); 
 	});
